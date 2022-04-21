89d67bf7f45d9ece7721331cbf133cc57f2ddf0b
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-04-21 16:53:15 +0300

    clear the git cache

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  BASE_URL:"https://api.github.com/search/users?q=",
-  base_repo_url: "https://api.github.com/search/repositories?q=",
-  access_token: "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",
-  single_user_url: "https://api.github.com/users/"
-};
