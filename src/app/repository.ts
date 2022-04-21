cb1efe399170d836d55ebe7613973cbff8d620cf
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-04-21 19:20:29 +0300

    Auto-generated commit [ci skip]

--- src/app/repository.ts
+++
@@ -1,6 +0,0 @@
-export class Repository {
-
-    constructor(public name:string, public description:string, public html_url:string, public created_at:Date, public language:string) {
-    
-    }
-}
--- src/app/user.ts
+++
@@ -1,7 +0,0 @@
-export class User {
-
-
-    constructor(public login:string, public followers:number, public following:number, public public_repos:number, public created_at:Date, public avatar_url:string, public html_url:string) {
-    
-    }
-}
