6b4be4aa413b5b0a42f97c6bb40d824ef3a12895
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-04-21 16:47:38 +0300

    add the changes in the gitignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   BASE_URL:"https://api.github.com/search/users?q=",
   base_repo_url: "https://api.github.com/search/repositories?q=",
-  access_token: "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",
+  access_token: "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",
   single_user_url: "https://api.github.com/users/"
 };
 
