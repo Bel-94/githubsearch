620f4bf50e5b410d4913d720e11baec4173a0f60
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-04-21 15:16:43 +0300

    add the changes in git ignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   BASE_URL:"https://api.github.com/search/users?q=",
   base_repo_url: "https://api.github.com/search/repositories?q=",
-  access_token: "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",
+  access_token: "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",
   single_user_url: "https://api.github.com/users/"
 };
 
